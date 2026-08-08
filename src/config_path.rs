564bc714339939db5053a41bbfaef652ae4b25ce
Benjamin Renz <benjamin.l.renz@gmail.com>
2026-08-08 23:36:51 +0200

    changed rendering to single string write

--- src/config_path.rs
+++ src/config_path.rs
@@ -1,7 +1,24 @@
-use directories::ProjectDirs;
 use std::path::PathBuf;
 
 /// Returns the path to the configuration directory
+
 pub fn config_dir() -> Option<PathBuf> {
-    ProjectDirs::from("", "", "termixel").map(|dirs| dirs.config_dir().to_path_buf())
+    #[cfg(target_os = "linux")]
+    {
+        std::env::var_os("XDG_CONFIG_HOME")
+            .map(PathBuf::from)
+            .or_else(|| std::env::var_os("HOME").map(|home| PathBuf::from(home).join(".config")))
+            .map(|path| path.join("termixel"))
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        std::env::var_os("HOME")
+            .map(|home| PathBuf::from(home).join("Library/Application Support/termixel"))
+    }
+
+    #[cfg(target_os = "windows")]
+    {
+        std::env::var_os("APPDATA").map(|appdata| PathBuf::from(appdata).join("termixel"))
+    }
 }
--- src/render_image.rs
+++ src/render_image.rs
@@ -1,17 +1,13 @@
-use std::io::{Write, stdout};
-
-use crossterm::{
-    execute,
-    style::{Color, Print, SetBackgroundColor, SetForegroundColor},
-};
 use image::RgbaImage;
+use std::fmt::Write as FmtWrite;
+use std::io::{Write, stdout};
 
 /// renders an image to the terminal using unicode characters
 pub fn render_image(image: &RgbaImage) -> Result<(), Box<dyn std::error::Error>> {
     let height = image.height();
     let width = image.width();
 
-    let mut stdout = stdout();
+    let mut output = String::with_capacity((width * height * 20) as usize);
 
     // Iterate over the pixels in the image, printing unicode characters to the terminal
     // Each pixel is a whole line on the x axis and a half line on the y axis
@@ -19,76 +15,52 @@ pub fn render_image(image: &RgbaImage) -> Result<(), Box<dyn std::error::Error>>
     for pixel_y in (0..height).step_by(2) {
         for pixel_x in 0..width {
             let pixel = image.get_pixel(pixel_x, pixel_y);
-            let pixel2 = image.get_pixel(pixel_x, pixel_y + 1);
+
+            let pixel2 = if pixel_y + 1 < height {
+                *image.get_pixel(pixel_x, pixel_y + 1)
+            } else {
+                image::Rgba([0, 0, 0, 0])
+            };
 
             let top_alpha = pixel[3];
             let bottom_alpha = pixel2[3];
 
             // Both pixels are not transparent, so print a block character
             if top_alpha > 0 && bottom_alpha > 0 {
-                let color_top = Color::Rgb {
-                    r: pixel[0],
-                    g: pixel[1],
-                    b: pixel[2],
-                };
-                let color_bottom = Color::Rgb {
-                    r: pixel2[0],
-                    g: pixel2[1],
-                    b: pixel2[2],
-                };
-
-                execute!(
-                    stdout,
-                    SetForegroundColor(color_top),
-                    SetBackgroundColor(color_bottom),
-                    Print("▀")
+                write!(
+                    output,
+                    "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m▀",
+                    pixel[0], pixel[1], pixel[2], pixel2[0], pixel2[1], pixel2[2],
                 )?;
             }
             // Top pixel is transparent, bottom pixel is not, so print a half block character
             else if top_alpha == 0 && bottom_alpha > 0 {
-                let color_bottom = Color::Rgb {
-                    r: pixel2[0],
-                    g: pixel2[1],
-                    b: pixel2[2],
-                };
-
-                execute!(
-                    stdout,
-                    SetForegroundColor(color_bottom),
-                    SetBackgroundColor(Color::Reset),
-                    Print("▄")
+                write!(
+                    output,
+                    "\x1b[38;2;{};{};{}m\x1b[49m▄",
+                    pixel2[0], pixel2[1], pixel2[2]
                 )?;
             }
             // Top pixel is not transparent, bottom pixel is transparent, so print a half block character
             else if top_alpha > 0 && bottom_alpha == 0 {
-                let color_top = Color::Rgb {
-                    r: pixel[0],
-                    g: pixel[1],
-                    b: pixel[2],
-                };
-
-                execute!(
-                    stdout,
-                    SetForegroundColor(color_top),
-                    SetBackgroundColor(Color::Reset),
-                    Print("▀")
+                write!(
+                    output,
+                    "\x1b[38;2;{};{};{}m\x1b[49m▀",
+                    pixel[0], pixel[1], pixel[2],
                 )?;
             }
             // Both pixels are transparent, so print a space character
             else {
-                execute!(
-                    stdout,
-                    SetBackgroundColor(Color::Reset),
-                    SetForegroundColor(Color::Reset),
-                    Print(" ")
-                )?;
+                write!(output, "\x1b[39m\x1b[49m ")?;
             }
         }
 
         // Print a newline character to move to the next line
-        execute!(stdout, Print("\n"))?;
+        output.push('\n');
     }
 
+    let mut stdout = stdout();
+    stdout.write_all(output.as_bytes())?;
     stdout.flush()?;
 
     Ok(())
